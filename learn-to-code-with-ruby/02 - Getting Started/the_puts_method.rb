178027b09fbd28f1d4bf7a2d479a4e95b4f95558
Abu Sufian <asufian47@gmail.com>
2023-04-28 12:18:57 +0900

    Puts Method

---
+++ learn-to-code-with-ruby/02 - Getting Started/the_puts_method.rb
@@ -0,0 +1,39 @@
+#puts 1 + "2" + "3" + "4" + "5" + "6" # String can't be coerced into Integer (TypeError)
+
+puts 1
+
+puts 1 + 2 # 3
+
+
+puts (1+2/3*5) # 1
+# Explanation: First, 2/3 will be evaluated, which evaluates to 0 because it is integer division. Next, 0*5 will be evaluated, which also equals 0. Finally, 1+0 will be evaluated, resulting in 1. 
+
+1 + 2 #
+# Ruby processing this code, but not output. Ruby will not ignore this line. Will process this line, get the number 8 and through it to out of the computer memory and got to next line. 
+
+puts (1 +3) # 4
+
+puts "Hello, World!"
+
+puts "How is everything going? "
+
+
+puts 3.14159 # 3.14159
+
+
+puts (1.0+2.0/3.0*5.0) # 4.333333333333333
+# First, it divides 2.0 by 3.0, which results in 0.6666666666666666. 
+# Then, it multiplies the result by 5.0, which gives 3.3333333333333335. 
+# Finally, it adds 1.0 to the result, which gives 4.333333333333333. 
+
+
+puts 5
+puts 3
+puts 7
+
+puts 5,4,3 # best approach to give space after each comma
+
+
+
+
+
